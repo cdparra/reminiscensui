7406ec5c775fc526db778f31e7ebf9f9e381664e
andrea <biasioliandrea@hotmail.it>
2012-11-06 15:42:21 +0100

    prova

---
+++ visualization/ProveTimeline/html/JavaScriptOverlay.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () { //lo carico all' apertura della pag
+
+    
+
+    $(".chiudi").click(
+    function () {
+        $('#overlay').fadeOut('fast');
+        $('#box').hide();
+    });
+
+    //chiusura emergenza 
+    $("#overlay").click(
+    function () {
+        $(this).fadeOut('fast');
+        $('#box').hide();
+    });
+
+
+
+});

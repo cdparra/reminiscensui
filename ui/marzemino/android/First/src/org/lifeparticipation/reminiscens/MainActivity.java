7347bce156776b52d3a9966d8f2485715fd6af55
Cristhian Parra <cdparra@gmail.com>
2013-08-02 13:53:37 +0200

    changed package name of android app wrapper

---
+++ ui/marzemino/android/First/src/org/lifeparticipation/reminiscens/MainActivity.java
@@ -0,0 +1,33 @@
+package org.lifeparticipation.reminiscens;
+
+import org.apache.cordova.DroidGap;
+
+import com.example.first.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index2.html");
+        super.setIntegerProperty("loadUrlTimeoutValue", 10000);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) 
+    { super.onConfigurationChanged(newConfig); }
+    
+}

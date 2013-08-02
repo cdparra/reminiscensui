6ef00db65edf6e530b8af65a5c4bd8bee3b8a491
Cristhian Parra <cdparra@gmail.com>
2013-08-02 14:00:52 +0200

    update mainActivity in android wrapper

--- ui/marzemino/android/First/src/org/lifeparticipation/reminiscens/MainActivity.java
+++ ui/marzemino/android/First/src/org/lifeparticipation/reminiscens/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends DroidGap {
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index2.html");
+        super.loadUrl("file:///android_asset/www/main.html");
         super.setIntegerProperty("loadUrlTimeoutValue", 10000);
     }
 

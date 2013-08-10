902b7db8393ea19559e660baf48e2ea3d7f35262
Cristhian Parra <cdparra@gmail.com>
2013-08-10 10:03:07 +0200

    small changes in android project

--- android/ReminiscensApp/src/org/lifeparticipation/reminiscens/MainActivity.java
+++ android/ReminiscensApp/src/org/lifeparticipation/reminiscens/MainActivity.java
@@ -14,10 +14,9 @@ public class MainActivity extends DroidGap {
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/main.html");
+        super.loadUrl("file:///android_asset/www/index.html");
         super.setIntegerProperty("loadUrlTimeoutValue", 10000);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
